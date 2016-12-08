7d5174adcc7dc0ec1b4e0b543bb8cf235ec43700
diabolo52 <diabolo52000@hotmail.fr>
2016-12-08 15:29:28 +0100

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -11,11 +11,23 @@ CREATE TABLE `security` (
   `event_time_stamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00')
    ENGINE=InnoDB DEFAULT CHARSET=latin1;
    
-   CREATE TABLE `membre` (
-   `id` int(11) NOT NULL,
-   `pseudo` varchar(255) NOT NULL,
-   `pass` varchar(255) NOT NULL,
-   `date_inscription` date NOT NULL )
-  ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-  
-  INSERT INTO `mysql`.`membre` (`id`, `pseudo`, `pass`, `date_inscription`) VALUES ('', 'root', '0000', CURDATE());
+
+
+CREATE TABLE `membre` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `date_inscription` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `membre`
+--
+
+INSERT INTO `membre` (`id`, `pseudo`, `pass`, `date_inscription`) VALUES
+(3, 'armen', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '2016-10-23'),
+(4, 'armen1', '011c945f30ce2cbafc452f39840f025693339c42', '2016-10-23'),
+(5, 'antonin', '39dfa55283318d31afe5a3ff4a0e3253e2045e43', '2016-10-23'),
+(6, 'root', '39dfa55283318d31afe5a3ff4a0e3253e2045e43', '2016-10-23'),
+(7, 'amaury', '39dfa55283318d31afe5a3ff4a0e3253e2045e43', '2016-10-24');
+
