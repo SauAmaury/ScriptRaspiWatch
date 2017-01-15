ff8697454bd9c49fc68a6ccf9ca4590b45aa26dc
diabolo52 <diabolo52000@hotmail.fr>
2017-01-15 23:20:51 +0100

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script: uninstall.sh
+# Purpose: Desinstalleur
+# Author:Amaury
+# -------------------------------------------------------
+
+sudo apt-get remove --yes --force-yes apache2
+sudo apt-get purge --yes --force-yes apache2
+
+sudo apt-get remove --yes --force-yes php5
+sudo apt-get purge --yes --force-yes php5
+
+
+sudo apt-get remove --yes --force-yes motion
+sudo apt-get purge --yes --force-yes motion
+
+sudo apt-get remove --yes --force-yes phpmyadmin
+sudo apt-get purge --yes --force-yes phpmyadmin
+
+sudo apt-get remove --yes --force-yes openssl
+sudo apt-get purge --yes --force-yes openssl
+
